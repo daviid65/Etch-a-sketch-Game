db6aa562ce4f036bbfc54e9ef39cf79b82260aea
daviid65 <david.tope@hotmail.com>
2023-04-21 14:12:03 +0200

    currently working a etch a sketch Game

--- myGame.js
+++ myGame.js
@@ -1,27 +1,64 @@
 const body = document.body; 
 const container = document.createElement("div");
 const gridContainer = document.createElement("div");
-gridContainer.setAttribute("style", "background-color: white; width: 450px; height: 450px; display: flex;")
+const btnContainer = document.createElement("div")
+const clearBtn = document.createElement("button");
+const createBtn = document.createElement("button");
 
-body.appendChild(container)
+
+body.appendChild(container);
+container.appendChild(btnContainer);
+btnContainer.appendChild(clearBtn);
+btnContainer.appendChild(createBtn);
 container.appendChild(gridContainer);
 container.setAttribute("id", "container")
+btnContainer.setAttribute("id", "btn-Container")
+
+clearBtn.textContent = "Clear"
+createBtn.textContent = "Create Grid"
+clearBtn.classList.add("btn")
+createBtn.classList.add("btn")
+gridContainer.classList.add("Grid")
 
 
-function createGrid() {
-    const row = prompt("how many rows do you want?")
-    const col = prompt("how many cols do you want?")
-
-    for (let i = 0; i < (col * row); i++) {
-        const rows = document.createElement("div");
-        const cols = document.createElement("div");
-        gridContainer.appendChild(rows);
-        gridContainer.appendChild(cols);
-        rows.classList.add("rowStyle")
-        cols.classList.add("colStyle")
-        console.log(rows)
-        console.log(cols)
+function createRow() {
+    const row = document.createElement("div");
+    let rows = prompt("How many rows?")
+        if(!Number(rows)) {
+        alert("sorry but the input isn't a number..")
+    } else if(Number(row)) {
+        for (let i = 0; i <= row; i++) {
+            gridContainer.appendChild(row);
+            row.classList.add("row")
+        }
+        console.log(row)
     }
 }
 
-createGrid()
+
+function createCol() {
+let cols = prompt("How many columns?")
+    if(!Number(cols)) {
+    alert("sorry but the input isn't a number..")
+    } else if(Number(col)) {
+        for (let i = 0; i <= col; i++) {
+            const col = document.createElement("div");
+            gridContainer.appendChild(col);
+            col.classList.add("col")
+        }
+        console.log(col)
+    }       
+}
+
+function createGrid() {
+   createRow()
+   createCol()
+}
+
+function clear() {
+
+}
+
+createBtn.addEventListener("click", createGrid)
+clearBtn.addEventListener("click", clear)
+
