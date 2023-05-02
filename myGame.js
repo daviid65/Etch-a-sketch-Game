32ffa8a771be37496fda860709b186ab9c9f944d
daviid65 <david.tope@hotmail.com>
2023-05-02 22:40:17 +0200

    Finished Etch a Sketch Game

--- myGame.js
+++ myGame.js
@@ -4,24 +4,20 @@ const grid = document.createElement("div");
 const btnContainer = document.createElement("div")
 const clearBtn = document.createElement("button");
 const createBtn = document.createElement("button");
-const eraserBtn = document.createElement("button");
 const squares = grid.querySelectorAll("div");
 squares.forEach((div) => div.remove())
-const color = document.querySelector(".ColorChange");
+const colorPicker = document.querySelector(".ColorChange");
 const ColorDiv = document.querySelector(".ColorGenerator");
 const settings = document.querySelector(".settings");
 const footer = document.getElementById("footer")
-
-
 
 body.appendChild(container);
 container.appendChild(settings);
 settings.appendChild(btnContainer);
 btnContainer.appendChild(clearBtn);
-btnContainer.appendChild(eraserBtn)
 btnContainer.appendChild(createBtn);
 container.appendChild(grid);
-ColorDiv.insertAdjacentElement("beforebegin", ColorDiv)
+settings.insertAdjacentElement("beforeend", ColorDiv)
 container.setAttribute("id", "container")
 btnContainer.setAttribute("id", "btn-Container")
 
@@ -29,28 +25,24 @@ clearBtn.textContent = "Clear"
 clearBtn.style.backgroundColor = "red";
 clearBtn.style.fontWeight = "bold"
 createBtn.textContent = "Create Grid"
-eraserBtn.textContent = "eraser"
 clearBtn.classList.add("btn")
 createBtn.classList.add("btn")
-eraserBtn.classList.add("btn")
-eraserBtn.classList.add("eraser")
 grid.classList.add("Grid")
 
 function changeColor() {
-    this.style.backgroundColor = "green"
+    const currentColor = colorPicker.value
+    if(currentColor !== '#000') {
+        this.style.backgroundColor = currentColor; 
+    } else {
+        this.style.backgroundColor = eraserBtn.value;
+    }
+    
 }
-
-
-function eraser() {
-
-}
-
 
 function clear() {
     grid.innerHTML = '';
     createGrid()
 }
-
 
 function createGrid() {
     let input = prompt("How many *Cols* x *Rows* do you want?")
@@ -66,13 +58,12 @@ function createGrid() {
         for (let i = 0; i <= gridSize; i++) {
             const square = document.createElement("div");
             square.setAttribute("style", "background-color: white; box-shadow: 0 0 5px black;")
-            grid.insertAdjacentElement("beforeend", square);
-            square.addEventListener("mouseover", changeColor)
-        }   
-    }        
-}
+            grid.insertAdjacentElement("beforeend", square); 
+            square.addEventListener("mouseover", changeColor); 
+              
+        }     
+    } 
+}   
 
 createBtn.addEventListener("click", createGrid);
 clearBtn.addEventListener("click", clear);
-
-
